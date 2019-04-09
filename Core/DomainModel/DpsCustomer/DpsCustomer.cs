925007aecb41ec3ca40be9d02b16a233c1ec2b28
H.Rashid <Development.dubai.7@jobfixers.be>
2019-04-09 18:32:09 +0400

    updates

---
+++ Core/DomainModel/DpsCustomer/DpsCustomer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using BoemmValueObjects;
+
+namespace Core.DomainModel.DpsCustomer
+{
+    public class DpsCustomer
+    {
+        [Key]
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public Email InvoiceEmail { get; set; }
+        public Email ContractsEmail { get; set; }
+        public bool BulkContractsEnabled { get; set; }
+        public virtual List<StatuteSettings> StatuteSettings { get; set; }
+        public InvoiceSettings invoiceSettings { get; set; }
+        public Contact Contact { get; set; }
+
+
+
+
+    }
+}
