bf70ac5fda9945a43bfde4728c92474dddd53650
Jannes Labaere <jannes.labaere@boemm.eu>
2019-04-18 08:48:40 +0000

    Added environment.stag.ts

---
+++ src/environments/environment.stag.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  dataFromAPI_JSON: true, // True = From Remote :: False = From JSON
+  dpsAPI: 'https://dpsapisstg.azurewebsites.net/api/',
+  boemmAPI : 'https://boemmapistg.azurewebsites.net/api/',
+  getCustomers: 'Customer',
+  getCustomerLists: 'customer/GetAllCustomersPreviewJson',
+  getJointCommitee: '',
+  getLegalForm: '',
+  getCounteries: '',
+  getParitairCommitee: 'ParitairCommitee',
+  getStatute: '',
+  getCodes: 'code'
+};
