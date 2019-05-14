d87cead4eca438a093b314116c96a980b205c220
Jobfixers <jobfixers@Jobfixerss-iMac.local>
2019-05-14 13:00:44 +0400

    bulk contract added

---
+++ src/app/views/bulk-contract/bulk-contract.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bulk-contract',
+  templateUrl: './bulk-contract.component.html',
+  styleUrls: ['./bulk-contract.component.css']
+})
+export class BulkContractComponent implements OnInit {
+
+  public showFormIndex = 1;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
