f08ad07345fb46037fd682fc80408f713c902559
Jannes Labaere <jannes.labaere@jobfixers.be>
2019-06-19 13:05:40 +0000

    Updated calendar.component.ts

--- src/app/componentcontrols/calendar/calendar.component.ts
+++ src/app/componentcontrols/calendar/calendar.component.ts
@@ -31,7 +31,7 @@ export class CalendarComponent implements OnInit {
 
     const today: Date = new Date();
     this.dataDropDown = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
-    this.dropDownMonth = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    this.dropDownMonth = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'october', 'november', 'december'];
     this.selectedYear = 0;
     this.dropDownYear = [];
 
