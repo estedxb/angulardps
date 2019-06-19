49e199e604d1be1361dd67cd6cb2b37169237095
Development.dubai.5 <development.dubai.5@jobfixers.be>
2019-06-19 14:59:22 +0400

    changes made

--- src/app/views/person/addperson/addperson.component.ts
+++ src/app/views/person/addperson/addperson.component.ts
@@ -34,6 +34,8 @@ export class AddPersonComponent implements OnInit {
   public recvdCountryOfBirth;
   public recvdCountryCode;
 
+  public zichmetdata;
+
   public selectedStatuteObject:any = {};
 
   public validSSID: boolean;
@@ -729,6 +731,10 @@ export class AddPersonComponent implements OnInit {
       this.AddPersonForm1.controls.telephoneNumber.setValue(data.person.phone.number);
     }
 
+    if(data.person !== null) {
+      this.zichmetdata = data.person.travelMode;
+    }    
+
   }
 
   loadObjects(response: any) {
@@ -825,10 +831,10 @@ export class AddPersonComponent implements OnInit {
     this.DpsPersonObject.person.socialSecurityNumber = this.PersonObject.socialSecurityNumber;
     this.DpsPersonObject.person.placeOfBirth = this.AddPersonForm1.get('placeOfBirth').value;
 
-
-
     this.DpsPersonObject.person.countryOfBirth = this.recvdCountryOfBirth; 
     this.DpsPersonObject.person.nationality = this.recvdNationalityString;
+
+    this.DpsPersonObject.person.travelMode = this.zichmetdata;
 
     this.DpsPersonObject.person.gender = new Gender();
     this.DpsPersonObject.person.gender.genderId = 0;
@@ -866,8 +872,7 @@ export class AddPersonComponent implements OnInit {
     this.DpsPersonObject.person.bankAccount.iban = this.AddPersonForm1.get('iban').value;
     this.DpsPersonObject.person.bankAccount.bic = this.AddPersonForm1.get('bic').value;
 
-    this.DpsPersonObject.person.travelMode = this.AddPersonForm1.get('travelMode').value;
-    this.DpsPersonObject.person.status = '';
+    this.DpsPersonObject.person.status = "";
 
     this.DpsPersonObject.statute = new Statute();
     this.DpsPersonObject.statute.name = this.selectedStatuteObject.name;
@@ -938,6 +943,19 @@ export class AddPersonComponent implements OnInit {
     }
   }
 
+
+  receiveZichMet($event) {
+
+    this.zichmetdata = $event;
+  
+    if(this.DpsPersonObject !== null && this.DpsPersonObject !== undefined)
+    if (this.DpsPersonObject.person !== undefined && this.DpsPersonObject.person !== null) {
+        this.DpsPersonObject.person.travelMode = $event;
+    }  
+
+    this.changeMessage();
+  
+    }
 
   onCountryReceiveNationality($event) {
 
